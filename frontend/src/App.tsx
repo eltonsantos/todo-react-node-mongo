4363dda614d18b65946bae804ad4dd1c368c6d57
Elton Santos <eltonaxl@hotmail.com>
2023-11-09 04:25:20 -0300

    Finish frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState, useRef, FormEvent } from "react"
+import { FiTrash } from "react-icons/fi"
+import { api } from "./services/api"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface CustomerProps {
+  id: string
+  name: string
+  email: string
+  status: boolean
+  created_at: string
 }
 
-export default App
+export function App() {
+
+  const [customers, setCustomers] = useState<CustomerProps[]>([])
+  const nameRef = useRef<HTMLInputElement | null>(null)
+  const emailRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    loadCustomers()
+  }, [])
+
+  async function loadCustomers() {
+    const response = await api.get("/customers")
+    setCustomers(response.data)
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    if (!nameRef.current?.value || !emailRef.current?.value) return
+
+    const response = await api.post("/customer", {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value
+    })
+
+    setCustomers(allCustomers => [...allCustomers, response.data])
+    nameRef.current.value = ''
+    emailRef.current.value = ''
+  }
+
+  async function handleDelete(id: string) {
+    try {
+      await api.delete("/customer", {
+        params: {
+          id
+        }
+      })
+
+      const allCustomers = customers.filter((customer) => customer.id !== id)
+
+      setCustomers(allCustomers)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2x1">
+        <h1 className="text-4x1 font-medium text-white">Clientes</h1>
+
+        <form className="flex flex-col my-6" onSubmit={handleSubmit}>
+          <label className="font-medium text-white">Nome:</label>
+          <input ref={nameRef} type="text" placeholder="Digite o nome" className="w-full mb-5 p-2 rounded" />
+
+          <label className="font-medium text-white">Email:</label>
+          <input ref={emailRef} type="text" placeholder="Digite o email" className="w-full mb-5 p-2 rounded" />
+
+          <input type="submit" value="Cadastrar" className="w-full cursor-pointer bg-green-500 p-2 rounded font-medium" />
+        </form>
+
+        <section className="flex flex-col gap-4">
+          {customers.map((customer) => (
+            <article key={customer.id} className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+              <p>
+                <span className="font-medium">Nome:</span>
+                {customer.name}
+              </p>
+              <p>
+                <span className="font-medium">Email:</span>
+                {customer.email}
+              </p>
+              <p>
+                <span className="font-medium">Status:</span>
+                {customer.status ? "ATIVO" : "INATIVO"}
+              </p>
+
+              <button onClick={() => handleDelete(customer.id)} className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+                <FiTrash size={18} color="#fff" />
+              </button>
+            </article>
+          ))}
+        </section>
+      </main>
+    </div>
+  )
+}
